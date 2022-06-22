60db49e7d89f8820fe5d8bb7d376e6396b757a6e
sergey leveyv <sargey7@gmail.com>
2022-06-22 12:20:54 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+
+const app = express();
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(process.env.PORT || 7000, () => {
+      console.log(`Server is running on port:${process.env.PORT || 7000}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();
