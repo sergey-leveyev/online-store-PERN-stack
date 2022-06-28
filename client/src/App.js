0f11d12bece28efe478c583dcbd54f7ff9a1db91
sergey leveyv <sargey7@gmail.com>
2022-06-28 14:02:20 +0300

    client

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div>WORKING</div>;
+}
+
+export default App;
---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Switch, Route,Redirect} from 'react-router-dom'
+
+const AppRouter = () => {
+  return (
+    <div>AppRouter</div>
+  )
+}
+
+export default AppRouter
