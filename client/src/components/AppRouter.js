0b687a649d1f4ce2a7c85d53784348a4bec4926f
sergey leveyv <sargey7@gmail.com>
2022-06-29 16:11:26 +0300

    routes start

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import {Switch, Route,Redirect} from 'react-router-dom'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { authRoutes, publicRoutes } from "../routes";
+import { SHOP_ROUTE } from "../utils/consts";
 
 const AppRouter = () => {
+  const isAuth = false;
   return (
-    <div>AppRouter</div>
-  )
-}
+    <Routes>
+      {isAuth &&
+        authRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} exact />
+        ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} exact />
+      ))}
+      <Route path="*" element={<Navigate replace to={SHOP_ROUTE} />} />
+    </Routes>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
